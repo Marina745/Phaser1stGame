89671c5c557da576a25cf0ae4b021cf2a7878664
Marina745 <158029971+Marina745@users.noreply.github.com>
2024-02-11 18:10:18 +0200

    3
    будуємо сцену

--- game.js
+++ game.js
@@ -2,14 +2,21 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
         update: update
     }
 };
-
 var game = new Phaser.Game(config);
+var platforms;
 
 function preload ()
 {
@@ -29,7 +36,14 @@ function create ()
     {
         this.add.image(400, 300, 'sky');
         this.add.image(400, 300, 'star');
-    } 
+        platforms = this.physics.add.staticGroup();
+
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
+    }
 }
 
 function update ()
